500a6627178c2107329966af1d4b6baf6bd05aa8
LMX Blade <devart.blake@gmail.com>
2026-02-23 23:17:47 -0500

    Migrate Batch 2A widgets to AnimationManager helpers

---
+++ lib/animations/animations.dart
@@ -0,0 +1,18 @@
+/// Central exports for shared animation primitives and widgets.
+///
+/// Import this file to consume animation utilities from a single location.
+library animations;
+
+export 'math/curves.dart';
+export 'math/transforms.dart';
+export 'particles/particle.dart';
+export 'particles/particle_field.dart';
+export 'particles/particle_field_painter.dart';
+export 'physics/constrained_scroll_physics.dart';
+export 'ui/animated_sprite.dart';
+export 'ui/blend_mask.dart';
+export 'ui/custom_paint_mask.dart';
+export 'ui/rotation_3d.dart';
+export 'ui/sprite.dart';
+export 'ui/widget_mask.dart';
+export 'ui/widget_model.dart';
