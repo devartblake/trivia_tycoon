fb87315225c44aea66adf016075525a934ba9285
LMX Blade <devart.blake@gmail.com>
2026-04-27 21:27:57 -0400

    Add go_router import to study session screen
    Import the go_router package in study_session_screen.dart so the file can use GoRouter navigation (e.g. context.go). No other functional changes included.

--- lib/screens/study_hub/study_session_screen.dart
+++ lib/screens/study_hub/study_session_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 
 import '../../core/dto/study_dto.dart';
 import '../../core/services/study/study_service.dart';
